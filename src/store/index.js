3b3e8b926850fcb867c2fc9c759eb8480b507525
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-06-29 15:34:33 +0200

    Splitting code

--- src/store/index.js
+++ src/store/index.js
@@ -1,41 +1,11 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import {  configureStore } from "@reduxjs/toolkit";
 
-const counterSlice = createSlice({
-	name: "counter",
-	initialState: { counter: 0, showCounter: true },
-	reducers: {
-		increment(state) {
-			state.counter++;
-		},
-		decrement(state) {
-			state.counter--;
-		},
-		increase(state, action) {
-			state.counter = state.counter + action.payload;
-		},
-		toggleHandler(state) {
-			state.showCounter = !state.showCounter;
-		},
-	},
-});
+import counterReducer from "./counter-slice";
+import authReducer from "./auth"
 
-const authSlice = createSlice({
-	name: "authentication",
-	initialState: { isAuthenticated: false },
-	reducers: {
-		login(state) {
-			state.isAuthenticated = true;
-		},
-		logout(state) {
-			state.isAuthenticated = false;
-		},
-	},
-});
 
 const store = configureStore({
-	reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+	reducer: { counter: counterReducer, auth: authReducer },
 });
 
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 export default store;
